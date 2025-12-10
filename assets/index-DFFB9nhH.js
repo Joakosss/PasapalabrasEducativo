b579c350e95d603c6e82e98957fab65600be2ff3
Joakosss <jo.perezu@duocuc.cl>
2025-12-10 17:04:43 -0300

    Updates

--- assets/index-DFFB9nhH.js
+++
@@ -1,9 +0,0 @@
-import{j as e,L as t}from"./index-_nK80RVC.js";import{c as r,d as s}from"./index-SdY77gKe.js";function l(){return e.jsxs("div",{className:"flex flex-col  lg:flex-row  items-center justify-center h-screen",children:[e.jsxs("div",{className:"md:w-[50%]",children:[e.jsx("h1",{className:"font-extrabold text-[clamp(4rem,5vw,5rem)] text-center text-blue-800",children:"Pasapalabras"}),e.jsx("h1",{className:"font-extrabold text-[clamp(4rem,5vw,5rem)] text-center text-blue-800",children:"Educativo"})]}),e.jsxs("div",{className:"flex flex-col items-center justify-center bg-white p-10 rounded-lg gap-2",children:[e.jsxs(t,{to:"/game",className:`\r
-          flex items-center gap-2 justify-center text-center\r
-          text-white bg-blue-500 text-4xl font-bold w-100 h-16 rounded-lg\r
-          hover:bg-blue-400 hover:text-blue-800\r
-          md:text-5xl md:h-24 md:w-120`,children:[e.jsx(r,{size:45}),"¡A Jugar!"]}),e.jsxs(t,{to:"/disenio",className:`\r
-          flex items-center gap-2 justify-center text-center\r
-          text-white bg-cyan-500 text-4xl font-bold w-100 h-16 rounded-lg\r
-          hover:bg-cyan-400 hover:text-cyan-700\r
-          md:text-5xl md:h-24 md:w-120`,children:[e.jsx(s,{size:45}),"Crear/Editar Rosco"]})]})]})}export{l as component};
